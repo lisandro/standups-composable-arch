854ff968ad1381e9757da31a149950bf3960adb8
Lisandro Falconi <4897773+lisandro@users.noreply.github.com>
2023-08-09 14:40:52 -0300

    Initial Commit

---
+++ Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Lisandro Falconi on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
