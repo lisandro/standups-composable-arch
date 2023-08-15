108b91829eba2b91a191fef4e95a2882c388f6e0
Lisandro Falconi <4897773+lisandro@users.noreply.github.com>
2023-08-15 14:46:32 -0300

    feat: Added ThemePicker view

---
+++ Standups/Views/ThemePicker.swift
@@ -0,0 +1,26 @@
+//
+//  ThemePicker.swift
+//  Standups
+//
+//  Created by Lisandro Falconi on 15/08/2023.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
+}
+
+#Preview {
+    ThemePicker(selection: .constant(.periwinkle))
+}
---
+++ Standups/Views/ThemeView.swift
@@ -0,0 +1,25 @@
+//
+//  ThemeView.swift
+//  Standups
+//
+//  Created by Lisandro Falconi on 15/08/2023.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(theme.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
