4b84428e65768ae38fdb368d302e115f1f80082c
Lisandro Falconi <4897773+lisandro@users.noreply.github.com>
2023-09-04 17:28:13 -0300

    feat: Add standup domain model and reducer on the list

--- Standups/Reducers/StandupsListFeature.swift
+++ Standups/Reducers/StandupsListFeature.swift
@@ -10,21 +10,29 @@ import Foundation
 
 struct StandupsListFeature: Reducer {
     struct State {
+        @PresentationState var addStandup: StandupFormFeature.State?
         var standups: IdentifiedArrayOf<Standup> = []
     }
     
     enum Action {
         case addButtonTapped
-        
+        case addStandup(PresentationAction<StandupFormFeature.Action>)
     }
+    
+    @Dependency(\.uuid) var uuid
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .addButtonTapped:
-                state.standups.append(Standup(id: UUID(), theme: .allCases.randomElement()!))
+                state.addStandup = StandupFormFeature.State(standup: Standup(id: self.uuid()))
+                return .none
+            case .addStandup:
                 return .none
             }
+        }
+        .ifLet(\.$addStandup, action: /Action.addStandup) {
+            StandupFormFeature()
         }
     }
 }
