cddf7932f64a8b65f030dba571815e7695ec0244
Lisandro Falconi <4897773+lisandro@users.noreply.github.com>
2023-12-11 13:40:49 -0300

    feat: Example of deep link with StackState defining the standup to go and enable edition

--- Standups/StandupsApp.swift
+++ Standups/StandupsApp.swift
@@ -10,10 +10,20 @@ import SwiftUI
 @main
 struct StandupsApp: App {
     var body: some Scene {
+        var editedStandup = Standup.mock
+        let _ = editedStandup.title += " Morning Sync"
         WindowGroup {
-            AppView(store: Store(initialState: AppFeature.State()){
-                AppFeature()
-            })
+            AppView(
+                store: Store(
+                    initialState: AppFeature.State(
+                        path: StackState([
+                            .detail(StandupDetailFeature.State(
+                                editStandup: StandupFormFeature.State(focus: .attendee(editedStandup.attendees[3].id),
+                                                                      standup: editedStandup),
+                                standup: .mock))
+                        ]), standupsList: StandupsListFeature.State(standups: [.mock]))){
+                            AppFeature()
+                        })
         }
     }
 }
